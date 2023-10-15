5f9cc87c82436f1a6b0d8914ca952f819eb17a75
Andriy Trofimov <andriy.trofimov@valtech.com>
2023-10-15 19:45:33 +0300

    display methods

---
+++ js/app.js
@@ -0,0 +1,85 @@
+class CalorieTracker {
+	constructor() {
+		this._calorieLimit = 1000;
+		this._totalCalories = 0;
+		this._meals = [];
+		this._workouts = [];
+
+		this._displayCaloriesLimit();
+		this._displayCaloriesTotal();
+		this._displayCaloriesConsumed();
+		this._displayCaloriesBurned();
+		this._displayCaloriesRemaining();
+	}
+
+	addMeal(meal) {
+		this._meals.push(meal);
+		this._totalCalories += meal.calories;
+		this._render();
+	}
+
+	addWorkout(workout) {
+		this._workouts.push(workout);
+		this._totalCalories -= workout.calories;
+		this._render();
+	}
+
+	_displayCaloriesTotal() {
+		const totalCaloriesEl = document.getElementById('calories-total');
+		totalCaloriesEl.innerHTML = this._totalCalories;
+	}
+
+	_displayCaloriesLimit() {
+		const limitCaloriesEl = document.getElementById('calories-limit');
+		limitCaloriesEl.innerHTML = this._calorieLimit;
+	}
+
+	_displayCaloriesConsumed() {
+		const caloriesConsumedEl = document.getElementById('calories-consumed');
+
+		const consumed = this._meals.reduce((total, meal) => total + meal.calories, 0);
+		caloriesConsumedEl.innerHTML = consumed;
+	}
+
+	_displayCaloriesBurned() {
+		const caloriesBurnedEl = document.getElementById('calories-burned');
+
+		const burned = this._workouts.reduce((total, workout) => total + workout.calories, 0);
+		caloriesBurnedEl.innerHTML = burned;
+	}
+
+	_displayCaloriesRemaining() {
+		const caloriesRemainingEl = document.getElementById('calories-remaining');
+		caloriesRemainingEl.innerHTML = this._calorieLimit - this._totalCalories;
+	}
+
+	_render() {
+		this._displayCaloriesTotal();
+		this._displayCaloriesConsumed();
+		this._displayCaloriesBurned();
+		this._displayCaloriesRemaining();
+	}
+}
+
+class Meal {
+	constructor(name, calories) {
+		this.id = Math.random().toString(16).slice(2);
+		this.name = name;
+		this.calories = calories;
+	}
+}
+
+class Workout {
+	constructor(name, calories) {
+		this.id = Math.random().toString(16).slice(2);
+		this.name = name;
+		this.calories = calories;
+	}
+}
+
+const tracker = new CalorieTracker();
+const breakfast = new Meal('Breakfast', 400);
+tracker.addMeal(breakfast);
+console.log(breakfast);
+const running = new Workout('Running', 200);
+tracker.addWorkout(running);
